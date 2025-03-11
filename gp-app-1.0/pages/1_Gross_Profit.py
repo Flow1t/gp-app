43988db16e720be2cf9cb7cf1a012100a0d6b007
Devin <120550670+Flow1t@users.noreply.github.com>
2025-03-11 10:52:57 +0700

    Update 1_Gross_Profit.py

--- gp-app-1.0/pages/1_Gross_Profit.py
+++ gp-app-1.0/pages/1_Gross_Profit.py
@@ -18,12 +18,12 @@ def process_gp(file1, file2):
     df2 = df2.iloc[:-1]
     
     # List of file names in your GitHub repository
-    file_urls = {
-        "df_prima": "https://raw.githubusercontent.com/Flow1t/gp-app/e8a3ac18f89b9afe3bd2184d66ccc497041a4ef3/DF%20Prima%20%202023%20dan%202024%20ACTUAL%20SUPPORT.xlsx",
-        "all_rs": "https://raw.githubusercontent.com/gp-app/e8a3ac18f89b9afe3bd2184d66ccc497041a4ef3/ALL%20RS.xlsx",
-        "pricelist": "https://raw.githubusercontent.com/Flow1t/gp-app/e8a3ac18f89b9afe3bd2184d66ccc497041a4ef3/Pricelist%20Car.xlsx",
-        "jabodetabek": "https://raw.githubusercontent.com/Flow1t/gp-app/e8a3ac18f89b9afe3bd2184d66ccc497041a4ef3/wilayah.txt",
-        "luar_jabodetabek": "https://raw.githubusercontent.com/Flow1t/gp-app/e8a3ac18f89b9afe3bd2184d66ccc497041a4ef3/luar%20jawa.txt"
+    file_urls = {gp-app-1.0/ALL RS.xlsx
+        "df_prima": "https://raw.githubusercontent.com/Flow1t/gp-app/gp-app-1.0/e8a3ac18f89b9afe3bd2184d66ccc497041a4ef3/DF%20Prima%20%202023%20dan%202024%20ACTUAL%20SUPPORT.xlsx",
+        "all_rs": "https://raw.githubusercontent.com/gp-app/gp-app-1.0/e8a3ac18f89b9afe3bd2184d66ccc497041a4ef3/ALL%20RS.xlsx",
+        "pricelist": "https://raw.githubusercontent.com/Flow1t/gp-app/gp-app-1.0/e8a3ac18f89b9afe3bd2184d66ccc497041a4ef3/Pricelist%20Car.xlsx",
+        "jabodetabek": "https://raw.githubusercontent.com/Flow1t/gp-app/gp-app-1.0/e8a3ac18f89b9afe3bd2184d66ccc497041a4ef3/wilayah.txt",
+        "luar_jabodetabek": "https://raw.githubusercontent.com/Flow1t/gp-app/gp-app-1.0/e8a3ac18f89b9afe3bd2184d66ccc497041a4ef3/luar%20jawa.txt"
     }
     
     # Function to download and read Excel files
