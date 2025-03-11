be1e0b34a595af269f44283f617c185f5c28b725
Devin <120550670+Flow1t@users.noreply.github.com>
2025-03-11 09:24:03 +0700

    Add files via upload
    main files for gp app

---
+++ gp-app-1.0/Main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="GP App"
+)
+
+st.write("# Wuling Monthly GP Report Generator")
+
+st.sidebar.success("Select a feature above")
+
+st.markdown(
+    """
+    Generate monthly financial report using "Gross Profit" report and Operational Expense report from "Laba Rugi" file.
+    \n\n
+    Feature Available:
+    - GP Generator
+    - Opex Summary Generator
+    - Opex per Cabang Generator
+"""
+)
+
