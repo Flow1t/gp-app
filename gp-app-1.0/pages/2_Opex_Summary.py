bb9391d6393e10e9ea7266ef68c8c38f466e7063
Flow1t <flowit000@gmail.com>
2025-08-05 14:42:35 +0700

    UPDATE: Opex Summary code updated

--- gp-app-1.0/pages/2_Opex_Summary.py
+++ gp-app-1.0/pages/2_Opex_Summary.py
@@ -91,7 +91,7 @@ def opex_summary(opex_file):
     aggregate_columns(opex_transpose, opex_sum_transpose, '(?i)outsourcing', 'Security & Cleaning Expense')
     aggregate_columns(opex_transpose, opex_sum_transpose, '(?i)perijinan', 'Claim Expense')
     aggregate_columns(opex_transpose, opex_sum_transpose, '(?i)buka puasa', 'Miscellanous')
-    aggregate_columns(opex_transpose, opex_sum_transpose, '(?i)provisi|credit', 'Bank Charge')
+    aggregate_columns(opex_transpose, opex_sum_transpose, '(?i)provisi|credit|transfer', 'Bank Charge')
 
     opex_sum_transpose['Total General & Administratif Expense'] = opex_sum_transpose['Salary Expense'] + opex_sum_transpose['Employee Welfare'] + opex_sum_transpose ['Jamsostek & Pension'] + opex_sum_transpose['Repair Maintenance'] + opex_sum_transpose ['Tools'] + opex_sum_transpose['Rent Expense'] + opex_sum_transpose['Entertainment & Representation Expense'] + opex_sum_transpose['Utilities Expense'] + opex_sum_transpose['Communication Expense'] + opex_sum_transpose['Transportation & Travelling Expense'] + opex_sum_transpose['Insurance Expense'] + opex_sum_transpose['Training Expense'] + opex_sum_transpose['Taxes License'] + opex_sum_transpose['Office Supplies'] + opex_sum_transpose['IT Expense'] + opex_sum_transpose['Security & Cleaning Expense'] + opex_sum_transpose['Recruitment'] + opex_sum_transpose['Uniform'] + opex_sum_transpose['Claim Expense'] + opex_sum_transpose['Bank Charge'] + opex_sum_transpose['Professional Fee'] + opex_sum_transpose['Miscellanous']
 
